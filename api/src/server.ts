409059d8345a6d3f0afc96b5c7adc833dd94fa13
Gustavo Campagnin <gcampagnin@gmail.com>
2025-10-29 10:36:00 -0300

    refactor: fix identation and update webhook list route

--- api/src/server.ts
+++ api/src/server.ts
@@ -10,6 +10,8 @@ import { fastifyCors } from '@fastify/cors'
 import ScalarApiReference from '@scalar/fastify-api-reference'
 import { listWebhooks } from './routes/list-webhooks'
 import { env } from './env'
+import { getWebhook } from './routes/get-webhook'
+import { deleteWebhook } from './routes/delete-webhook'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -38,6 +40,8 @@ app.register(ScalarApiReference, {
 })
 
 app.register(listWebhooks)
+app.register(getWebhook)
+app.register(deleteWebhook)
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running on http://localhost:3333!')
